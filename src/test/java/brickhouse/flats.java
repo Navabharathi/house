3209d0b09a174a468c93ea2f62b2b28e11a694df
Navabharathi <navabharath@gmail.com>
2021-07-25 22:04:46 -0700

    Merge & git is completed

--- src/test/java/brickhouse/flats.java
+++ src/test/java/brickhouse/flats.java
@@ -5,6 +5,7 @@ public class flats {
 	public static void main(String[] args) {
 		System.out.println("update completed");
 		System.out.println("Merge done");
+		System.out.println("Git completed");
 
 	}
 
